da9f2c08d0ff1be6ce05ccbbdee0e71ac26e14a0
sanyadrian <adrianovoleksandr@gmail.com>
2023-04-12 20:55:03 -0400

    The last commit

--- PetBlog/static/js/readmore.js
+++ PetBlog/static/js/readmore.js
@@ -1,6 +1,28 @@
-const description = document.querySelector('.post1__description');
-const readMoreButton = document.querySelector('.read-more-button');
+const readMoreButtons = document.querySelectorAll('.read-more-button');
+const postDetails = document.querySelectorAll('.post1__details');
 
-readMoreButton.addEventListener('click', function() {
-  description.classList.toggle('expanded');
+postDetails.forEach(function(postDetail) {
+    const readMoreButton = postDetail.querySelector('.read-more-button');
+    const description = postDetail.querySelector('.post1__description');
+
+    readMoreButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        description.classList.toggle('expanded');
+        readMoreButton.textContent = description.classList.contains('expanded') ? 'Read less' : 'Read more';
+    });
 });
+
+//readMoreButton.addEventListener('click', function(e) {
+//  if (e.target.classList.contains('read-more-button')) {
+//    e.target.classList.toggle('expanded');
+//
+//    if (e.target.classList.contains('expanded')) {
+//      description.style.height = 'auto';
+//      e.target.textContent = 'Read less';
+//    } else {
+//      description.style.height = '100px';
+//      e.target.textContent = 'Read more';
+//      e.target.remove('expanded')
+//    }
+//  }
+//});
